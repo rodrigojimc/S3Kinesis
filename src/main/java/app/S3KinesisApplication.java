4453f93f193e2381598ad35d08e350525813c3a5
Rodrigo Jiménez Correa <rodrigo.jimenez@ksquareinc.com>
2020-03-03 08:37:32 -0600

    Init

---
+++ src/main/java/app/S3KinesisApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3KinesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3KinesisApplication.class, args);
+	}
+
+}
